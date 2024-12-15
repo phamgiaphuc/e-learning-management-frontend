ccafa2eeb04722d83d645f2db3708d5dc1fc0638
Acus <acus1304pgphcm@gmail.com>
2024-12-15 15:57:24 +0700

    Phuc | Update code

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -1,5 +1,7 @@
 const envConfig = {
-  serverUrl: import.meta.env.VITE_SERVER_URL,
+  serverUrl:
+    import.meta.env.VITE_SERVER_URL ||
+    "https://elearning-hcmiu.azurewebsites.net/api/v1",
 } as const;
 
 export default envConfig;
