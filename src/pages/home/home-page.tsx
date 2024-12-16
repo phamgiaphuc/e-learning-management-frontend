64a974472e6dfa280fceeb0138f489af8cdb3d2e
Acus <acus1304pgphcm@gmail.com>
2024-12-16 22:03:20 +0700

    Phuc | Update page

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -168,7 +168,7 @@ const HomePage = () => {
             </Typography>
           </Box>
         )}
-        <Stack gap={2} sx={{ width: "100%" }}>
+        <Stack gap={2} sx={{ width: "100%", marginBottom: 4 }}>
           {isAuthenticated ? (
             user?.role === "user" ? (
               <CourseStudent />
